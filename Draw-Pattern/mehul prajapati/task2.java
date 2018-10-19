dabbde669b087686392c3cca91b3044ec7655ce0
mehuls85345 <diduknow16@gmail.com>
2018-10-19 17:25:26 +0530

    regular expression

---
+++ Draw-Pattern/mehul prajapati/task2.java
@@ -0,0 +1,16 @@
+class task2
+{
+public static void main(String arg[])
+{
+int k=1;
+  for(int i=0;i<6;i++)
+		 {
+		  for(int j=0;j<i;j++,k++)
+		  {
+							System.out.print(k + " ");
+		  }
+						System.out.println();
+		 }
+}
+
+}
